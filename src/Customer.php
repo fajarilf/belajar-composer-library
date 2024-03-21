35bbb75989de70d756a28f4e7f79e8c7c79ea3eb
fajarilf <fjr.ilf.firmansyah@gmail.com>
2024-03-21 14:18:03 +0700

    contoh library composer

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ArgekPeople\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello customer $name, my name is $this->name";
+    }
+}
